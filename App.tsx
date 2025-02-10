d514f69037366be2f87ca0aae37bd0b218275765
Louis Loiseau-Billon <l.loiseau-billon@groupeonepoint.com>
2025-02-10 15:26:05 +0100

    add date-time picker repro

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const open = () => {
+    DateTimePickerAndroid.open({
+      accessibilityLabel: "Birthdate",
+      "aria-label": "Birthdate",
+      "aria-modal": true,
+      value: new Date(),
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable onPress={open}>
+        <Text>Open date-time picker</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
